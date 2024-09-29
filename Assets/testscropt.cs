28537a46e8ee6aa7d651686c8c9165ae617046fb
Arnold Miranda-Pino <167902301+4RN1E@users.noreply.github.com>
2024-09-28 22:11:44 -0400

    test

--- Assets/testscropt.cs
+++ Assets/testscropt.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class testscropt : MonoBehaviour
 {
+    public GameObject testThing;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        testThing.SetActive(false);
     }
 
     // Update is called once per frame
