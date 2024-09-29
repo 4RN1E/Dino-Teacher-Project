f38b0496a22c6b6590b7265731d010f27ecc52bf
imkemely <kemelyam@gmail.com>
2024-09-29 06:43:36 -0400

    Audio Updates

--- Assets/Kemely/AudioManager.cs
+++ Assets/Kemely/AudioManager.cs
@@ -4,10 +4,37 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public AudioSource backgroundMusic;
+    public AudioSource correctSound;
+    public AudioSource incorrectSound;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //background music
+        if (backgroundMusic != null)
+        {
+            backgroundMusic.loop = true;
+            backgroundMusic.Play(); 
+           
+        }
+      
+    }
+
+    public void PlayCorrectSound()
+    {
+        if (correctSound != null)
+        {
+            correctSound.Play();
+        }
+    }
+
+    public void PlayIncorrectSound()
+    {
+        if (incorrectSound != null)
+        {
+            incorrectSound.Play();
+        }
     }
 
     // Update is called once per frame
